c93f81f1e5a82cb151012107bb4381c3774b7dd0
Joey Gaffney <joeygaffney22@gmail.com>
2022-08-22 17:21:36 -0700

    nav scroll

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+// nav scroll
+
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener('scroll', () => {
+  const currentScroll = window.pageYOffset
+
+  if (currentScroll <= 0) {
+    body.classList.remove("scroll-up")
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-up")
+    body.classList.add("scroll-down")
+  }
+  
+  if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-down")
+    body.classList.add("scroll-up")
+  }
+
+  lastScroll = currentScroll;
+});
+
+
+// toggle
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 const navbarBar = document.getElementsByClassName('navbar')[0]
@@ -7,6 +35,7 @@ toggleButton.addEventListener('click', () => {
     navbarBar.classList.toggle('active')
 })
 
+// hero img reveal
 
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
